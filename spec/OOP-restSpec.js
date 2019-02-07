2750cfba2289150c7fbf7c4a92414b38d5d86967
TPL9 <tpl9@Techtonicas-MacBook-Air-6.local>
2019-02-07 15:44:53 -0800

    create jasmine test boiler

---
+++ spec/OOP-restSpec.js
@@ -0,0 +1,18 @@
+describe("Calculator", function() {
+    var Calculator = require('../app/OOP-rest');
+    var calculator;
+  
+    beforeEach(function() {
+      calculator = new Calculator();
+    });
+  
+    describe("Does program have restaurants?", function() {
+      it("returns name of a restaurant", function() {
+        expect(calculator.add("")).toEqual(0);
+      });
+      xit("returns name of a user", function() {
+        expect(calculator.add("2")).toEqual(2);
+      });
+    });
+  });
+  
