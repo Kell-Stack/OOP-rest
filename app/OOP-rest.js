df64f99e1a72d37f7739681f1cadf5be52056b05
TPL9 <tpl9@Techtonicas-MacBook-Air-6.local>
2019-02-07 13:32:06 -0800

    shell

---
+++ app/OOP-rest.js
@@ -0,0 +1,23 @@
+// // Classes
+
+// //Users - recommenders & inquirers
+
+// class User {
+//     constructor (rec, inq)
+//         this.rec = rec;
+//         this.inq - inq;
+// }
+
+// //Ratings
+
+// class Rating {
+//     constructor (minStarRating)
+// }
+
+// //Reccomendations
+
+// class sugRests 
+
+// // Restaurants in system
+
+// class Rests
